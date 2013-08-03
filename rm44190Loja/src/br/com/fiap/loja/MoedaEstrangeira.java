79e4bc7b88d4a49166cf4359be69db03bbacd6ff
Renata Hidalgo <garciahidalgo@gmail.com>
2013-08-03 14:36:15 -0300

    Versão inicial do Freeshop

---
+++ rm44190Loja/src/br/com/fiap/loja/MoedaEstrangeira.java
@@ -0,0 +1,21 @@
+package br.com.fiap.loja;
+
+/**
+ *  MoedaEstrangeira. Classe responsável pelo tratamento da moeda americana
+    @author Renata Hidalgo
+ */
+public class MoedaEstrangeira {
+
+	/**
+	 * Converter valor monetário de dolar para real.
+	 *
+	 * @param valor valor em dolar
+	 * @return valor em real
+	 */
+	public int converterDolar(int valor){
+		if (valor <0) return 0; 
+		return valor*2;
+		
+	}
+		
+}
